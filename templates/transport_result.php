f8f1d908c66616b1b5ab36b511881854cdb80323
Hamid Hosen Azad <hamidxazad@gmail.com>
2025-03-20 14:58:41 +0600

    wordpress issues fixed till Variables and options must be escaped when echo'd

--- templates/transport_result.php
+++ templates/transport_result.php
@@ -1,5 +1,10 @@
 <?php
-$http_referrer = isset($_SERVER['HTTP_REFERER']) ? esc_url(sanitize_url($_SERVER['HTTP_REFERER'])) : ''; 
+// First sanitize the URL for storage/processing
+$raw_url = isset($_SERVER['HTTP_REFERER']) ? wp_unslash($_SERVER['HTTP_REFERER']) : '';
+$sanitized_url = esc_url_raw($raw_url);
+
+// Then escape it for output
+$http_referrer = esc_url($sanitized_url);
 
 $inline_script = '(function($) {';
 $inline_script .= '"use strict";';
