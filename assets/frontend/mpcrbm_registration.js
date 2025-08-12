f04d592dee795558de778572c8bdb13b99fcde69
sjrubel10 <rubelcuet10@gmail.com>
2025-08-12 13:51:03 +0600

    set default page for search result

--- assets/frontend/mpcrbm_registration.js
+++ assets/frontend/mpcrbm_registration.js
@@ -342,7 +342,7 @@ jQuery(document).ready(function($) {
                 ).done(function (startCoordinates, endCoordinates) {
                     if (start_place.value && end_place.value && start_date && start_time && return_date && return_time) {
                         let actionValue;
-                        if ( !mpcrbm_enable_view_search_result_page) {
+                        if ( mpcrbm_enable_view_search_result_page == 'No' ) {
                             actionValue = "mpcrbm_get_map_search_result";
                             
                             $.ajax({
@@ -428,7 +428,7 @@ jQuery(document).ready(function($) {
                 if (start_place.value && end_place.value && start_date && start_time && return_date && return_time) {
                     
                     let actionValue;
-                    if ( !mpcrbm_enable_view_search_result_page) {
+                    if ( mpcrbm_enable_view_search_result_page === 'No' ) {
                         actionValue = "mpcrbm_get_map_search_result";
                        
                         $.ajax({
