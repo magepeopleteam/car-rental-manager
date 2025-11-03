96d10ee703a7442659ab6ebf078a344bae6c30f9
rubelcuet10@gmail.com <you@example.com>
2025-11-03 09:49:43 +0600

    remove car info tab

--- admin/MPCRBM_Taxonomies.php
+++ admin/MPCRBM_Taxonomies.php
@@ -337,7 +337,6 @@ if (!class_exists('MPCRBM_Taxonomies')) {
                         <button class="mpcrbm_taxonomies_tab" data-target="mpcrbm_car_feature"><i class="mi mi-list-timeline"></i> <?php esc_attr_e( 'Car Feature', 'car-rental-manager' );?></button>
                         <button class="mpcrbm_taxonomies_tab" data-target="mpcrbm_manage_faq"><i class="mi mi-messages-question"></i> <?php esc_attr_e( 'Manage Faq', 'car-rental-manager' );?></button>
                         <button class="mpcrbm_taxonomies_tab" data-target="mpcrbm_manage_term_condition"><i class="mi mi-blog-text"></i> <?php esc_attr_e( 'Manage Term & Condition', 'car-rental-manager' );?></button>
-                        <button class="mpcrbm_taxonomies_tab" data-target="mpcrbm_manage_car_info"><i class="mi mi-blog-text"></i> <?php esc_attr_e( 'Car Info', 'car-rental-manager' );?></button>
 
                     </div>
                 </div>
@@ -433,12 +432,6 @@ if (!class_exists('MPCRBM_Taxonomies')) {
                         <div class="mpcrbm_taxonomies_content_holder" id="mpcrbm_manage_term_condition_holder" style="display: none">
                             <?php
                                 MPCRBM_Manage_Faq::term_and_condition_display();
-                            ?>
-                        </div>
-                        <div class="mpcrbm_taxonomies_content_holder" id="mpcrbm_manage_car_info_holder" style="display: none">
-                            <?php
-//                                MPCRBM_Manage_Faq::term_and_condition_display();
-                            echo 'car info';
                             ?>
                         </div>
                     </div>
